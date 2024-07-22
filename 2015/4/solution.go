db1baba11a7e40071042d3cc9c5151b1ac448e3e
Lincoln Simmons <lincolnsimmons@gmail.com>
2024-07-22 02:46:42 -0500

    Go solution to 2015 Day 4 (MD5 Sums)

---
+++ 2015/4/solution.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+)
+
+func testHash5(hash [16]byte) bool {
+	// We check first three bytes. Looking for 5 nibbles to be zero so...
+	// hash = 00 00 0X XX..
+	if hash[0] != 0 || hash[1] != 0 || hash[2]&0xF0 != 0 {
+		return false
+	}
+
+	return true
+}
+
+func testHash6(hash [16]byte) bool {
+	// We check first three bytes. Looking for 6 nibbles to be zero so...
+	// hash = 00 00 00 XX..
+	if hash[0] != 0 || hash[1] != 0 || hash[2] != 0 {
+		return false
+	}
+
+	return true
+}
+func main() {
+	// The example 'abcdef' should md5 hash with int '609043' to produce
+	// five leading zeros: md5sum('abcdef609043') = 000001dbbfa...
+	//key := "abcdef"
+	//key := "pqrstuv" should find '1048970'
+	key := "bgvyzdsv"
+
+	/* Start with 1... */
+	test := 1
+
+	fmt.Println("Part 1:")
+	for {
+		hash := md5.Sum([]byte(key + strconv.Itoa(test)))
+		if testHash5(hash) {
+			fmt.Printf("Found MD5(%s%s) = %v\n", key, strconv.Itoa(test), hash)
+			break
+		}
+		test += 1
+	}
+
+	/* Start with 1... */
+	test = 1
+
+	fmt.Println("Part 2:")
+	for {
+		hash := md5.Sum([]byte(key + strconv.Itoa(test)))
+		if testHash6(hash) {
+			fmt.Printf("Found MD5(%s%s) = %v\n", key, strconv.Itoa(test), hash)
+			break
+		}
+		test += 1
+	}
+}
